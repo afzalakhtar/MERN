40c4f09775a7eed290d81838fd25853c15073072
Afzl Akhtar <afzalakhtar-at-134595404889>
2023-12-13 17:44:17 +0530

    js

--- JS-ludo-game/js/script.js
+++ JS-ludo-game/js/script.js
@@ -2,10 +2,8 @@ console.log('JavaScript is loaded!');
 
 
 
-let elDiceOne = document.getElementById('dice');
-let elComeOut = document.getElementById('roll');
-const player1 = document.querySelector('#player1');
-const player2 = document.querySelector('#player2'), result = document.querySelector('#result');
+let elDiceOne = document.getElementById('dice'), elComeOut = document.getElementById('roll');
+const player1 = document.querySelector('#player1'), player2 = document.querySelector('#player2');
 let activePlayer = document.querySelector('.player.active');
 // console.log(activePlayer)
 
@@ -18,13 +16,13 @@ function rollDice(e) {
 
     // console.log(diceOne);
 
-
+    
 
     // activePlayer.classList.remove('active');
     var player1Score, player2Score, diceOne, dicetwo;
     if (player1.classList.contains('active')) {
         diceOne = Math.floor((Math.random() * 6) + 1);
-        for (var i = 1; i <= 6; i++) {
+        for (let i = 1; i <= 6; i++) {
             elDiceOne.classList.remove('show-' + i);
             if (diceOne === i) {
                 elDiceOne.classList.add('show-' + i);
@@ -32,13 +30,10 @@ function rollDice(e) {
         }
         player1Score = diceOne;
         console.log(player1Score);
-        setTimeout(() => {
-            player1.querySelector('.score').innerHTML = player1Score;
-        }, 1500)
-
+        player1.querySelector('.score').innerHTML = player1Score;
     } else if (player2.classList.contains('active')) {
         dicetwo = Math.floor((Math.random() * 6) + 1);
-        for (var i = 1; i <= 6; i++) {
+        for (let i = 1; i <= 6; i++) {
             elDiceOne.classList.remove('show-' + i);
             if (dicetwo === i) {
                 elDiceOne.classList.add('show-' + i);
@@ -46,33 +41,27 @@ function rollDice(e) {
         }
         player2Score = dicetwo;
         console.log(player2Score);
+        player2.querySelector('.score').innerHTML = player2Score;
 
-        setTimeout(() => {
-            player2.querySelector('.score').innerHTML = player2Score;
-        }, 1500)
+        
 
-        setTimeout(() => {
-            if (player1Score > player2Score) {
-                console.log('Player 1 won!');
-                result.innerText = 'Player 1 won!';
-            } else if (player1Score < player2Score) {
-                console.log('Player 2 won!');
-                result.innerText = 'Player 2 won!';
-            } else {
-                console.log('Draw!');
-                result.innerText = 'Draw!';
-            }
-        }, 2000)
+        // if(player1Score > player2Score){
+        //     console.log('Player 1 won!');
+        // }else if(player1Score < player2Score){
+        //     console.log('Player 2 won!');
+        // }else{
+        //     console.log('Draw!');
+        // }
     } else {
         // The element does not have the class 'active'.
     }
     console.log(`Player 1: ${player1Score} and Player 2: ${player2Score}`)
 
-    setTimeout(() => {
-        player1.classList.toggle('active');
-        player2.classList.toggle('active');
-    }, 1750)
+    
 
+
+    player1.classList.toggle('active');
+    player2.classList.toggle('active');
 
     // activePlayer.querySelector('.score').innerHTML = diceOne + diceOne;
     e.preventDefault();
