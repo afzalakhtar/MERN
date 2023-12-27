bd9ac269cd5eae9475768f11d326f2c8146cc525
Md Afzal Akhtar <afzalakhtar@gmail.com>
2023-12-28 01:10:43 +0530

    Slick slider added in home page

--- quick-eat/src/Components/Home.js
+++ quick-eat/src/Components/Home.js
@@ -1,7 +1,9 @@
+import Hero from "./Hero";
 
 const Home = () => {
     return (
         <div className="container mx-auto px-5">
+            <Hero />
             <h1>Home Page</h1>
         </div>
     )
